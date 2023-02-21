91148981770f236bcb5d2d59d8996a02db2d3931
JameshaRicketts <jamesharicketts@gmail.com>
2023-02-21 22:30:32 +0000

    done

---
+++ problem1.sql
@@ -0,0 +1,2 @@
+SELECT sum(salary) FROM employee;
+--#### Problem 1: use the `SUM()` aggregate function to output the total of all salaries found in the table.
---
+++ problem3.sql
@@ -0,0 +1,2 @@
+SELECT MIN(salary) FROM employee;
+--#### Problem 3: use the `MIN()` aggregate function to return the lowest salary.
